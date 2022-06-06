70dbab2885918af1b9f2a1b6a22484ccbed75043
Karthickkoppaka <karthickkoppaka@gmail.com>
2022-06-06 01:21:14 +0100

    setting django heroku

--- core/settings.py
+++ core/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +130,5 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
