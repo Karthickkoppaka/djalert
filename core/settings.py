8458aeabdda43ff49d16e74ed409fae67a5c2387
Karthickkoppaka <karthickkoppaka@gmail.com>
2022-06-23 18:58:28 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -129,6 +129,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static')
 
 django_heroku.settings(locals())
